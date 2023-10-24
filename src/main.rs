352b38331ee873cd1f0827cee8a1a41e4cf9660e
bLeadDev <59748268+bLeadDev@users.noreply.github.com>
2023-10-24 09:02:12 +0000

    Added Module

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, ops::Range};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
@@ -14,7 +14,61 @@ fn main() {
 
 pub fn is_isogram(value: &str) -> bool {
     //implementation goes here!
-    false
+    if value.len() == 0 {
+        // empty strings are always isograms
+        return true;
+    }
+
+    let mut counts : Vec<i32> = vec![0;256];
+    for c in 0..255 {
+        for cc in value.chars() {
+            let ccc = cc.to_ascii_lowercase();
+            if c == (ccc as usize) {
+                counts[ccc as usize] +=  1;
+            }            
+        }
+    }
+
+    let mut letter_count = 0;
+    for count in counts {
+        if count > 0 && letter_count == 0{
+            //init, first run
+            letter_count = count;
+        }else if  count > 0{
+            if letter_count != count{
+                return false;
+            }
+        }
+    }
+    return true;
+
+    //       c
+    //   o   t    t   o
+    //   ^             
+/* 
+
+    let mut last_count = None;
+
+    for c in value.chars() {
+        let mut count = 0;
+        
+        for cc in value.chars() {
+            if c == cc {
+                count = count + 1;
+            }
+        }
+
+        if let Some(x) = last_count {
+            if x != count {
+                return false;
+            }
+        }else {
+            //first character
+            last_count = Some(count);
+        }
+    }*/
+
+    true
 }
 
 #[cfg(test)]
@@ -23,7 +77,7 @@ mod isogram_tests {
 
     #[test]
     fn empty_string_should_not_be_an_isogram() {
-        assert!(!is_isogram(""));
+        assert!(is_isogram("otto"));
     }
 
     #[test]
